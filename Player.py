6d126fd3b8af364881301df686f4f41c346516f9
unknown <bzheng99.real@gmail.com>
2022-09-26 14:34:56 -0500

    minor changes

--- Player.py
+++ Player.py
@@ -3,45 +3,48 @@
 
 class Player:
     def __init__(self):
-        self.name = None;
-        self.uid = None;
-        self.numPlayersHit = 0;
-        self.gotHit = False;
-        self.count_down = 20; # Countdown timer for vest to reactivate
-        self.alive = True;
+        self.name = None
+        self.uid = None
+        self.numPlayersHit = 0
+        self.gotHit = False
+        self.count_down = 20  # Countdown timer for vest to reactivate
+        self.alive = True
+        self.numLives = 1  # numLives was originally declared in subtractLife(),
+        # set to default in initialization for now
+        self.numHits = 0
 
     def setName(self, name):
-        self.name = name;
+        self.name = name
 
     def setUID(self, UID):
-        self.uid = UID;
+        self.uid = UID
 
     def subtractLife(self):
-        self.numLives = self.numLives - 1;
+        self.numLives = self.numLives - 1
 
     def gotHit(self):
-        self.gotHit = True;
+        self.gotHit = True
 
     def hitOtherPlayer(self):
-        self.numPlayersHit = self.numHits + 1;
+        self.numPlayersHit = self.numHits + 1
 
     def countdown(self):
-        if self.gotHit == True:
+        if self.gotHit is True:
             if self.count_down > 0:
-                self.count_down = self.count_down - 1;
+                self.count_down = self.count_down - 1
             else:
-                self.gotHit = False;
-                self.count_down = 20;
+                self.gotHit = False
+                self.count_down = 20
     
     def toggleLife(self):
         if self.numLives > 0:
-            pass;
+            pass
         else:
-            self.alive = False;
+            self.alive = False
 
     def update(self):
-        self.toggleLife();
-        self.countdown();
+        self.toggleLife()
+        self.countdown()
         
         
     
