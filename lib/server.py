fdd2fe51abca54aabdfe15e95c2282bcae685da2
levic08 <levicrider08@gmail.com>
2022-11-09 13:05:09 -0600

    Class update

--- lib/server.py
+++ lib/server.py
@@ -1,20 +1,26 @@
 import socket
 
+class Server:
+    def __init__(self):
+        # We will want to set game is on to false once the timer runs out.
+        self.GameIsOn = True;
+        self.address = "127.0.0.1"
+        self.port = 7501 
+        self.bufferSize = 1024
 
-# We will want to set game is on to false once the timer runs out.
-GameIsOn = True;
-address = "127.0.0.1"
-port = 7501
-bufferSize = 1024
+    # Create our socket
+    def createSocket(self,address,port):
+        UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM);
+        # Binding the server to our IP
+        UDPServerSocket.bind((address, port));
+        print("UDP Server is up a running");
+        return UDPServerSocket;
 
-# Create our socket
-UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-
-# Binding the server to our IP
-UDPServerSocket.bind((address, port));
-print("UDP Server is up a running");
-
-while(GameIsOn):
-    # Info 
-    info = UDPServerSocket.recvfrom(bufferSize);
-    print(info[0])
+    # Call this method after creating our server object in main.py
+    def runServer(self):
+        server = self.createSocket(self.address, self.port);
+        while self.GameIsOn:
+            info = server.recvfrom(self.bufferSize);
+            print(info[0]);
+server = Server();
+server.runServer();
